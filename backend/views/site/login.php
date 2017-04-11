be5480283131a1459783c1f6b8d1cf132bdadd9c
ElisDN <mail@elisdn.ru>
2017-04-11 12:42:51 +0300

    Simplified backend login page

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -50,20 +50,7 @@ $fieldOptions2 = [
             <!-- /.col -->
         </div>
 
-
         <?php ActiveForm::end(); ?>
-
-        <div class="social-auth-links text-center">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in
-                using Facebook</a>
-            <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign
-                in using Google+</a>
-        </div>
-        <!-- /.social-auth-links -->
-
-        <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
 
     </div>
     <!-- /.login-box-body -->
