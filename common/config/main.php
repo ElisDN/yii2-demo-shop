3d830ebbefd5e46ba208765d7516925305629ec6
ElisDN <mail@elisdn.ru>
2017-04-11 11:21:08 +0300

    Migrated to asset-packagist

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,10 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
