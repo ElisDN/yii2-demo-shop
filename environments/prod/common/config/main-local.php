266c14e055b047980e4cfd32d8a270397580ba07
ElisDN <mail@elisdn.ru>
2017-05-11 19:55:15 +0300

    Removed suport email

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -11,6 +11,9 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'messageConfig' => [
+                'from' => ['support@example.com' => 'Shop']
+            ],
         ],
     ],
 ];
