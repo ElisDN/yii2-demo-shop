a3045a8253d805e15672549a6a5c3e1cd1b95a8e
ElisDN <mail@elisdn.ru>
2017-05-11 18:32:36 +0300

    Added signup service

---
+++ frontend/services/auth/SignupService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\services\auth;
+
+use common\entities\User;
+use frontend\forms\SignupForm;
+
+class SignupService
+{
+    public function signup(SignupForm $form): User
+    {
+        $user = User::signup(
+            $form->username,
+            $form->email,
+            $form->password
+        );
+
+        if (!$user->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+
+        return $user;
+    }
+}
--- frontend/tests/unit/forms/SignupFormTest.php
+++ frontend/tests/unit/forms/SignupFormTest.php
@@ -30,13 +30,7 @@ class SignupFormTest extends \Codeception\Test\Unit
             'password' => 'some_password',
         ]);
 
-        $user = $model->signup();
-
-        expect($user)->isInstanceOf('common\entities\User');
-
-        expect($user->username)->equals('some_username');
-        expect($user->email)->equals('some_email@example.com');
-        expect($user->validatePassword('some_password'))->true();
+        expect_that($model->validate());
     }
 
     public function testNotCorrectSignup()
@@ -47,7 +41,7 @@ class SignupFormTest extends \Codeception\Test\Unit
             'password' => 'some_password',
         ]);
 
-        expect_not($model->signup());
+        expect_not($model->validate());
         expect_that($model->getErrors('username'));
         expect_that($model->getErrors('email'));
 
