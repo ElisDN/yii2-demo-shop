293396dd0aec2d674737cd8d53c52e12a11ded80
ElisDN <mail@elisdn.ru>
2017-05-11 19:11:54 +0300

    Added password reset service

---
+++ frontend/services/auth/PasswordResetService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace frontend\services\auth;
+
+use common\entities\User;
+use frontend\forms\PasswordResetRequestForm;
+use frontend\forms\ResetPasswordForm;
+use Yii;
+
+class PasswordResetService
+{
+    public function request(PasswordResetRequestForm $form): void
+    {
+        /* @var $user User */
+        $user = User::findOne([
+            'status' => User::STATUS_ACTIVE,
+            'email' => $form->email,
+        ]);
+
+        if (!$user) {
+            throw new \DomainException('User is not found.');
+        }
+
+        $user->requestPasswordReset();
+
+        if (!$user->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+
+        $sent = Yii::$app
+            ->mailer
+            ->compose(
+                ['html' => 'passwordResetToken-html', 'text' => 'passwordResetToken-text'],
+                ['user' => $user]
+            )
+            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setTo($user->email)
+            ->setSubject('Password reset for ' . Yii::$app->name)
+            ->send();
+
+        if (!$sent) {
+            throw new \RuntimeException('Sending error.');
+        }
+    }
+
+    public function validateToken($token): void
+    {
+        if (empty($token) || !is_string($token)) {
+            throw new \DomainException('Password reset token cannot be blank.');
+        }
+        if (!User::findByPasswordResetToken($token)) {
+            throw new \DomainException('Wrong password reset token.');
+        }
+    }
+
+    public function reset(string $token, ResetPasswordForm $form): void
+    {
+        $user = User::findByPasswordResetToken($token);
+
+        if (!$user) {
+            throw new \DomainException('User is not found.');
+        }
+
+        $user->resetPassword($form->password);
+
+        if (!$user->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+}
