58f1c864a42df503d614a4c8f223ed343bf82225
ElisDN <mail@elisdn.ru>
2017-05-11 19:32:07 +0300

    Added contact service

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace frontend\controllers;
 use frontend\services\auth\PasswordResetService;
 use Yii;
 use frontend\services\auth\SignupService;
+use frontend\services\contact\ContactService;
 use Yii;
 use yii\base\InvalidParamException;
 use yii\web\BadRequestHttpException;
@@ -22,11 +23,18 @@ use frontend\forms\ContactForm;
 class SiteController extends Controller
 {
     private $passwordResetService;
+    private $contactService;
 
-    public function __construct($id, $module, PasswordResetService $passwordResetService, $config = [])
+    public function __construct(
+        $id,
+        $module,
+        PasswordResetService $passwordResetService,
+        ContactService $contactService,
+        $config = [])
     {
         parent::__construct($id, $module, $config);
         $this->passwordResetService = $passwordResetService;
+        $this->contactService = $contactService;
     }
 
     /**
@@ -126,19 +134,21 @@ class SiteController extends Controller
      */
     public function actionContact()
     {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
+        $form = new ContactForm();
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $this->contactService->send($form);
                 Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
-            } else {
+                return $this->goHome();
+            } catch (\Exception $e) {
+                Yii::$app->errorHandler->logException($e);
                 Yii::$app->session->setFlash('error', 'There was an error sending your message.');
             }
-
             return $this->refresh();
         }
 
         return $this->render('contact', [
-            'model' => $model,
+            'model' => $form,
         ]);
     }
 
--- frontend/forms/ContactForm.php
+++ frontend/forms/ContactForm.php
@@ -5,9 +5,6 @@ namespace frontend\forms;
 use Yii;
 use yii\base\Model;
 
-/**
- * ContactForm is the model behind the contact form.
- */
 class ContactForm extends Model
 {
     public $name;
@@ -16,45 +13,19 @@ class ContactForm extends Model
     public $body;
     public $verifyCode;
 
-
-    /**
-     * @inheritdoc
-     */
     public function rules()
     {
         return [
-            // name, email, subject and body are required
             [['name', 'email', 'subject', 'body'], 'required'],
-            // email has to be a valid email address
             ['email', 'email'],
-            // verifyCode needs to be entered correctly
             ['verifyCode', 'captcha'],
         ];
     }
 
-    /**
-     * @inheritdoc
-     */
     public function attributeLabels()
     {
         return [
             'verifyCode' => 'Verification Code',
         ];
-    }
-
-    /**
-     * Sends an email to the specified email address using the information collected by this model.
-     *
-     * @param string $email the target email address
-     * @return bool whether the email was sent
-     */
-    public function sendEmail($email)
-    {
-        return Yii::$app->mailer->compose()
-            ->setTo($email)
-            ->setFrom([$this->email => $this->name])
-            ->setSubject($this->subject)
-            ->setTextBody($this->body)
-            ->send();
     }
 }
