504f0ab367654468f0049a98ac0f2e5e138b23f4
ElisDN <mail@elisdn.ru>
2017-05-11 20:05:00 +0300

    Added email confirmation
    Added email confirmation

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -22,17 +22,20 @@ use frontend\forms\ContactForm;
  */
 class SiteController extends Controller
 {
+    private $signupService;
     private $passwordResetService;
     private $contactService;
 
     public function __construct(
         $id,
         $module,
+        SignupService $signupService,
         PasswordResetService $passwordResetService,
         ContactService $contactService,
         $config = [])
     {
         parent::__construct($id, $module, $config);
+        $this->signupService = $signupService;
         $this->passwordResetService = $passwordResetService;
         $this->contactService = $contactService;
     }
@@ -171,15 +174,32 @@ class SiteController extends Controller
     {
         $form = new SignupForm();
         if ($form->load(Yii::$app->request->post()) && $form->validate()) {
-            $user = (new SignupService())->signup($form);
-            if (Yii::$app->getUser()->login($user)) {
+            try {
+                $this->signupService->signup($form);
+                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
                 return $this->goHome();
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
             }
         }
 
         return $this->render('signup', [
             'model' => $form,
         ]);
+    }
+
+    public function actionConfirm($token)
+    {
+        try {
+            $this->signupService->confirm($token);
+            Yii::$app->session->setFlash('success', 'Your email is confirmed.');
+            return $this->redirect(['login']);
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+            return $this->goHome();
+        }
     }
 
     /**
