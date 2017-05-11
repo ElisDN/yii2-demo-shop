7eeaf06b2f9aebc1c2b1274b290d52f4466bf871
ElisDN <mail@elisdn.ru>
2017-05-11 16:15:40 +0300

    Added user constructor

---
+++ common/tests/unit/entities/User/SignupTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace common\tests\unit\entities\User;
+
+use Codeception\Test\Unit;
+use common\entities\User;
+
+class SignupTest extends Unit
+{
+    public function testSuccess()
+    {
+        $user = new User(
+            $username = 'username',
+            $email = 'email@site.com',
+            $password = 'password'
+        );
+
+        $this->assertEquals($username, $user->username);
+        $this->assertEquals($email, $user->email);
+        $this->assertNotEmpty($user->password_hash);
+        $this->assertNotEquals($password, $user->password_hash);
+        $this->assertNotEmpty($user->created_at);
+        $this->assertNotEmpty($user->auth_key);
+        $this->assertEquals(User::STATUS_ACTIVE, $user->status);
+    }
+}
