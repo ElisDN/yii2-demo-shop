7973f8a14c7636ff57d258aebf673eec8d6322e9
ElisDN <mail@elisdn.ru>
2017-05-13 11:52:37 +0300

    Added catalog product layout

---
+++ frontend/views/shop/catalog/product.php
@@ -0,0 +1,216 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'HP LP3065';
+$this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="row">
+    <div class="col-sm-8">
+        <ul class="thumbnails">
+            <li><a class="thumbnail" href="http://static.shop.dev/cache/products/hp_1-500x500.jpg" title="HP LP3065"><img src="http://static.shop.dev/cache/products/hp_1-228x228.jpg" title="HP LP3065" alt="HP LP3065" /></a></li>
+            <li class="image-additional"><a class="thumbnail" href="http://static.shop.dev/cache/products/hp_3-500x500.jpg" title="HP LP3065"> <img src="http://static.shop.dev/cache/products/hp_3-74x74.jpg" title="HP LP3065" alt="HP LP3065" /></a></li>
+            <li class="image-additional"><a class="thumbnail" href="http://static.shop.dev/cache/products/hp_2-500x500.jpg" title="HP LP3065"> <img src="http://static.shop.dev/cache/products/hp_2-74x74.jpg" title="HP LP3065" alt="HP LP3065" /></a></li>
+        </ul>
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#tab-description" data-toggle="tab">Description</a></li>
+            <li><a href="#tab-specification" data-toggle="tab">Specification</a></li>
+            <li><a href="#tab-review" data-toggle="tab">Reviews (0)</a></li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane active" id="tab-description"><p>
+                    Stop your co-workers in their tracks with the stunning new 30-inch diagonal HP LP3065 Flat Panel Monitor. This flagship monitor features best-in-class performance and presentation features on a huge wide-aspect screen while letting you work as comfortably as possible - you might even forget you&#39;re at the office</p>
+            </div>
+            <div class="tab-pane" id="tab-specification">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <td colspan="2"><strong>Memory</strong></td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>test 1</td>
+                        <td>16GB</td>
+                    </tr>
+                    </tbody>
+                    <thead>
+                    <tr>
+                        <td colspan="2"><strong>Processor</strong></td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>No. of Cores</td>
+                        <td>4</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="tab-pane" id="tab-review">
+                <form class="form-horizontal" id="form-review">
+                    <div id="review"></div>
+                    <h2>Write a review</h2>
+                    <div class="form-group required">
+                        <div class="col-sm-12">
+                            <label class="control-label" for="input-name">Your Name</label>
+                            <input type="text" name="name" value="" id="input-name" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="form-group required">
+                        <div class="col-sm-12">
+                            <label class="control-label" for="input-review">Your Review</label>
+                            <textarea name="text" rows="5" id="input-review" class="form-control"></textarea>
+                            <div class="help-block"><span class="text-danger">Note:</span> HTML is not translated!</div>
+                        </div>
+                    </div>
+                    <div class="form-group required">
+                        <div class="col-sm-12">
+                            <label class="control-label">Rating</label>
+                            &nbsp;&nbsp;&nbsp; Bad&nbsp;
+                            <input type="radio" name="rating" value="1" />
+                            &nbsp;
+                            <input type="radio" name="rating" value="2" />
+                            &nbsp;
+                            <input type="radio" name="rating" value="3" />
+                            &nbsp;
+                            <input type="radio" name="rating" value="4" />
+                            &nbsp;
+                            <input type="radio" name="rating" value="5" />
+                            &nbsp;Good</div>
+                    </div>
+                    <div class="buttons clearfix">
+                        <div class="pull-right">
+                            <button type="button" id="button-review" data-loading-text="Loading..." class="btn btn-primary">Continue</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <p class="btn-group">
+            <button type="button" data-toggle="tooltip" class="btn btn-default" title="Add to Wish List" onclick="wishlist.add('47');"><i class="fa fa-heart"></i></button>
+            <button type="button" data-toggle="tooltip" class="btn btn-default" title="Compare this Product" onclick="compare.add('47');"><i class="fa fa-exchange"></i></button>
+        </p>
+        <h1>HP LP3065</h1>
+        <ul class="list-unstyled">
+            <li>Brand: <a href="/index.php?route=product/manufacturer/info&amp;manufacturer_id=7">Hewlett-Packard</a></li>
+            <li>Product Code: Product 21</li>
+            <li>Reward Points: 300</li>
+            <li>Availability: In Stock</li>
+        </ul>
+        <ul class="list-unstyled">
+            <li>
+                <h2>$122.00</h2>
+            </li>
+            <li>Ex Tax: $100.00</li>
+            <li>Price in reward points: 400</li>
+        </ul>
+        <div id="product">
+            <hr>
+            <h3>Available Options</h3>
+            <div class="form-group required">
+                <label class="control-label" for="input-option226">Select</label>
+                <select name="option[226]" id="input-option226" class="form-control">
+                    <option value=""> --- Please Select --- </option>
+                    <option value="15">Red</option>
+                    <option value="16">Blue</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="input-quantity">Qty</label>
+                <input type="text" name="quantity" value="1" size="2" id="input-quantity" class="form-control" />
+                <input type="hidden" name="product_id" value="47" />
+                <br />
+                <button type="button" id="button-cart" data-loading-text="Loading..." class="btn btn-primary btn-lg btn-block">Add to Cart</button>
+            </div>
+        </div>
+        <div class="rating">
+            <p>
+                <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
+                <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
+                <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
+                <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
+                <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
+                <a href="" onclick="$('a[href=\'#tab-review\']').trigger('click'); return false;">0 reviews</a> / <a href="" onclick="$('a[href=\'#tab-review\']').trigger('click'); return false;">Write a review</a></p>
+            <hr>
+            <!-- AddThis Button BEGIN -->
+            <div class="addthis_toolbox addthis_default_style" data-url="/index.php?route=product/product&amp;product_id=47"><a class="addthis_button_facebook_like" fb:like:layout="button_count"></a> <a class="addthis_button_tweet"></a> <a class="addthis_button_pinterest_pinit"></a> <a class="addthis_counter addthis_pill_style"></a></div>
+            <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-515eeaf54693130e"></script>
+            <!-- AddThis Button END -->
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $('#button-cart').on('click', function() {
+        $.ajax({
+            url: 'index.php?route=checkout/cart/add',
+            type: 'post',
+            data: $('#product input[type=\'text\'], #product input[type=\'hidden\'], #product input[type=\'radio\']:checked, #product input[type=\'checkbox\']:checked, #product select, #product textarea'),
+            dataType: 'json',
+            beforeSend: function() {
+                $('#button-cart').button('loading');
+            },
+            complete: function() {
+                $('#button-cart').button('reset');
+            },
+            success: function(json) {
+                $('.alert, .text-danger').remove();
+                $('.form-group').removeClass('has-error');
+
+                if (json['error']) {
+                    if (json['error']['option']) {
+                        for (i in json['error']['option']) {
+                            var element = $('#input-option' + i.replace('_', '-'));
+
+                            if (element.parent().hasClass('input-group')) {
+                                element.parent().after('<div class="text-danger">' + json['error']['option'][i] + '</div>');
+                            } else {
+                                element.after('<div class="text-danger">' + json['error']['option'][i] + '</div>');
+                            }
+                        }
+                    }
+
+                    if (json['error']['recurring']) {
+                        $('select[name=\'recurring_id\']').after('<div class="text-danger">' + json['error']['recurring'] + '</div>');
+                    }
+
+                    // Highlight any found errors
+                    $('.text-danger').parent().addClass('has-error');
+                }
+
+                if (json['success']) {
+                    $('.breadcrumb').after('<div class="alert alert-success">' + json['success'] + '<button type="button" class="close" data-dismiss="alert">&times;</button></div>');
+
+                    $('#cart > button').html('<span id="cart-total"><i class="fa fa-shopping-cart"></i> ' + json['total'] + '</span>');
+
+                    $('html, body').animate({ scrollTop: 0 }, 'slow');
+
+                    $('#cart > ul').load('index.php?route=common/cart/info ul li');
+                }
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            }
+        });
+    });
+</script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.thumbnails').magnificPopup({
+            type:'image',
+            delegate: 'a',
+            gallery: {
+                enabled:true
+            }
+        });
+    });
+</script>
+
+
+
