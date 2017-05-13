872c86e5050923c8459f565df604026a7684b00a
ElisDN <mail@elisdn.ru>
2017-05-13 19:54:35 +0300

    Added user create action

---
+++ shop/services/manage/UserManageService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace shop\services\manage;
+
+use shop\entities\User\User;
+use shop\forms\manage\User\UserCreateForm;
+use shop\repositories\UserRepository;
+
+class UserManageService
+{
+    private $repository;
+
+    public function __construct(UserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function create(UserCreateForm $form): User
+    {
+        $user = User::create(
+            $form->username,
+            $form->email,
+            $form->password
+        );
+        $this->repository->save($user);
+        return $user;
+    }
+}
