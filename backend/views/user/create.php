41cb264d16960a02a9f33196000b9d45653c4826
ElisDN <mail@elisdn.ru>
2017-05-13 18:59:32 +0300

    Removed unused h1

--- backend/views/user/create.php
+++ backend/views/user/create.php
@@ -1,8 +1,5 @@
 <?php
 
-use yii\helpers\Html;
-
-
 /* @var $this yii\web\View */
 /* @var $model shop\entities\User */
 
@@ -11,8 +8,6 @@ $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
