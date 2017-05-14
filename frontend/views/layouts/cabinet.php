022d35b29395c27a9baa10dd441ac7f3ea9caccc
ElisDN <mail@elisdn.ru>
2017-05-14 16:30:23 +0300

    Changed cabinet links

--- frontend/views/layouts/cabinet.php
+++ frontend/views/layouts/cabinet.php
@@ -3,6 +3,9 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
+use yii\helpers\Html;
+use yii\helpers\Url;
+
 ?>
 <?php $this->beginContent('@frontend/views/layouts/main.php') ?>
 
@@ -12,9 +15,9 @@
     </div>
     <aside id="column-right" class="col-sm-3 hidden-xs">
         <div class="list-group">
-            <a href="/account/login" class="list-group-item">Login</a>
-            <a href="/account/register" class="list-group-item">Register</a>
-            <a href="/account/forgotten" class="list-group-item">Forgotten Password</a>
+            <a href="<?= Html::encode(Url::to(['/auth/auth/login'])) ?>" class="list-group-item">Login</a>
+            <a href="<?= Html::encode(Url::to(['/auth/signup/request'])) ?>" class="list-group-item">Signup</a>
+            <a href="<?= Html::encode(Url::to(['/auth/reset/request'])) ?>" class="list-group-item">Forgotten Password</a>
             <a href="/account/account" class="list-group-item">My Account</a>
             <a href="/account/wishlist" class="list-group-item">Wish List</a>
             <a href="/account/order" class="list-group-item">Order History</a>
