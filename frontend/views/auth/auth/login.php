0a13692bfe4af82f1c8125a46e81308ebd9e6f4f
ElisDN <mail@elisdn.ru>
2017-05-14 16:38:18 +0300

    Changed login page

--- frontend/views/auth/auth/login.php
+++ frontend/views/auth/auth/login.php
@@ -6,39 +6,50 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
 
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['auth/reset/request']) ?>.
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
-
-            <h2>Socials</h2>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="well">
+            <h2>New Customer</h2>
+            <p><strong>Register Account</strong></p>
+            <p>By creating an account you will be able to shop faster, be up to date on an order's status,
+                and keep track of the orders you have previously made.</p>
+            <a href="<?= Html::encode(Url::to(['/auth/signup/request'])) ?>" class="btn btn-primary">Continue</a>
+        </div>
+        <div class="well">
+            <h2>Social Login</h2>
             <?= yii\authclient\widgets\AuthChoice::widget([
                 'baseAuthUrl' => ['auth/network/auth']
             ]); ?>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="well">
+            <h2>Returning Customer</h2>
+            <p><strong>I am a returning customer</strong></p>
+
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+            <?= $form->field($model, 'username')->textInput() ?>
+
+            <?= $form->field($model, 'password')->passwordInput() ?>
+
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+            <div style="color:#999;margin:1em 0">
+                If you forgot your password you can <?= Html::a('reset it', ['auth/reset/request']) ?>.
+            </div>
+
+            <div>
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
