0fe909160e2015d814321e4bff56a68560cdfc80
ElisDN <mail@elisdn.ru>
2017-05-15 10:16:17 +0300

    Installed Memcached

--- common/config/main.php
+++ common/config/main.php
@@ -7,8 +7,8 @@ return [
     ],
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
-            'cachePath' => '@common/runtime/cache',
+            'class' => 'yii\caching\MemCache',
+            'useMemcached' => true,
         ],
     ],
 ];
--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -34,7 +34,7 @@ apt-get update
 apt-get upgrade -y
 
 info "Install additional software"
-apt-get install -y php7.1-curl php7.1-cli php7.1-intl php7.1-mysqlnd php7.1-gd php7.1-fpm php7.1-mbstring php7.1-xml unzip nginx mysql-server-5.7
+apt-get install -y php7.1-curl php7.1-cli php7.1-intl php7.1-mysqlnd php7.1-gd php7.1-fpm php7.1-mbstring php7.1-xml unzip nginx mysql-server-5.7 php7.1-memcached memcached
 
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
