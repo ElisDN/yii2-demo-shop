e2fb65b607318fcf8df167fc89e6b30674fffdaf
ElisDN <mail@elisdn.ru>
2017-05-15 10:10:34 +0300

    Unified cache

--- environments/index.php
+++ environments/index.php
@@ -32,6 +32,7 @@ return [
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
+            'common/runtime',
             'backend/runtime',
             'backend/web/assets',
             'frontend/runtime',
@@ -48,6 +49,7 @@ return [
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
+            'common/runtime',
             'backend/runtime',
             'backend/web/assets',
             'frontend/runtime',
