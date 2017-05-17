8bad1e74e88f44329d04a66b5756c2bcabfcbe40
ElisDN <mail@elisdn.ru>
2017-05-17 12:22:00 +0300

    Added user networks model

---
+++ console/migrations/m170517_084404_change_users_field_requirements.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Migration;
+
+class m170517_084404_change_users_field_requirements extends Migration
+{
+    public function up()
+    {
+        $this->alterColumn('{{%users}}', 'username', $this->string());
+        $this->alterColumn('{{%users}}', 'password_hash', $this->string());
+        $this->alterColumn('{{%users}}', 'email', $this->string());
+    }
+
+    public function down()
+    {
+        $this->alterColumn('{{%users}}', 'username', $this->string()->notNull());
+        $this->alterColumn('{{%users}}', 'password_hash', $this->string()->notNull());
+        $this->alterColumn('{{%users}}', 'email', $this->string()->notNull());
+    }
+}
