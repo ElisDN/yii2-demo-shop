e8b95668f22098846797818912aab14418374e32
ElisDN <mail@elisdn.ru>
2017-05-17 11:24:25 +0300

    Renamed user table

--- shop/entities/User/User.php
+++ shop/entities/User/User.php
@@ -81,7 +81,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return '{{%users}}';
     }
 
     /**
