7cf7880c2dd33161af2c72de80a7af0d64bc32e1
ElisDN <mail@elisdn.ru>
2017-05-17 13:49:59 +0300

    Added cabinet controller

---
+++ frontend/controllers/cabinet/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace frontend\controllers\cabinet;
+
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
---
+++ frontend/views/cabinet/default/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Cabinet';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="cabinet-index">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Hello!</p>
+</div>
