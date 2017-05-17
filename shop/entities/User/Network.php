d8c377148d2682cbbe5fc9e76952cb251258708d
ElisDN <mail@elisdn.ru>
2017-05-17 12:22:00 +0300

    Added user networks model

---
+++ shop/entities/User/Network.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace shop\entities\User;
+
+use Webmozart\Assert\Assert;
+use yii\db\ActiveRecord;
+
+/**
+ * @property integer $user_id
+ * @property string $identity
+ * @property string $network
+ */
+class Network extends ActiveRecord
+{
+    public static function create($network, $identity): self
+    {
+        Assert::notEmpty($network);
+        Assert::notEmpty($identity);
+
+        $item = new static();
+        $item->network = $network;
+        $item->identity = $identity;
+        return $item;
+    }
+
+    public static function tableName()
+    {
+        return '{{%user_networks}}';
+    }
+}
