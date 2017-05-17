d30f821b7620a6984e3f50cb940e30fca356706b
ElisDN <mail@elisdn.ru>
2017-05-17 14:43:24 +0300

    Added network attachement

---
+++ frontend/controllers/cabinet/NetworkController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace frontend\controllers\cabinet;
+
+use shop\services\auth\NetworkService;
+use Yii;
+use yii\authclient\ClientInterface;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
+use yii\web\Controller;
+use yii\authclient\AuthAction;
+
+class NetworkController extends Controller
+{
+    private $service;
+
+    public function __construct($id, $module, NetworkService $service, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->service = $service;
+    }
+
+    public function actions()
+    {
+        return [
+            'attach' => [
+                'class' => AuthAction::class,
+                'successCallback' => [$this, 'onAuthSuccess'],
+                'successUrl' => Url::to(['cabinet/default/index']),
+            ],
+        ];
+    }
+
+    public function onAuthSuccess(ClientInterface $client): void
+    {
+        $network = $client->getId();
+        $attributes = $client->getUserAttributes();
+        $identity = ArrayHelper::getValue($attributes, 'id');
+
+        try {
+            $this->service->attach(Yii::$app->user->id, $network, $identity);
+            Yii::$app->session->setFlash('success', 'Network is successfully attached.');
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+    }
+}
