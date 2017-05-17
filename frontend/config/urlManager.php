3e692a17449cc6068fcdc8e90e081f542d959ecb
ElisDN <mail@elisdn.ru>
2017-05-17 13:49:59 +0300

    Added cabinet controller

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -15,6 +15,12 @@ return [
         'signup/<_a:[\w-]+>' => 'auth/signup/<_a>',
         '<_a:login|logout>' => 'auth/auth/<_a>',
 
+        'cabinet' => 'cabinet/default/index',
+        'cabinet/<_c:[\w\-]+>' => 'cabinet/<_c>/index',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>' => 'cabinet/<_c>/view',
+        'cabinet/<_c:[\w\-]+>/<_a:[\w-]+>' => 'cabinet/<_c>/<_a>',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => 'cabinet/<_c>/<_a>',
+
         '<_c:[\w\-]+>' => '<_c>/index',
         '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
         '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
