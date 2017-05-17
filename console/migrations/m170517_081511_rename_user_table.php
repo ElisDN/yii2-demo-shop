2af06b6e19b37d6ca6c86502900b87bda4e7b818
ElisDN <mail@elisdn.ru>
2017-05-17 11:24:25 +0300

    Renamed user table

---
+++ console/migrations/m170517_081511_rename_user_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m170517_081511_rename_user_table extends Migration
+{
+    public function up()
+    {
+        $this->renameTable('{{%user}}', '{{%users}}');
+    }
+
+    public function down()
+    {
+        $this->renameTable('{{%users}}', '{{%user}}');
+    }
+}
