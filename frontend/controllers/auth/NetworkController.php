c0aa2fe125447ad26c90b41f4303446febce50f7
ElisDN <mail@elisdn.ru>
2017-05-17 13:15:45 +0300

    Added social login controller

---
+++ frontend/controllers/auth/NetworkController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace frontend\controllers\auth;
+
+use shop\services\auth\NetworkService;
+use Yii;
+use yii\authclient\ClientInterface;
+use yii\helpers\ArrayHelper;
+use yii\web\Controller;
+use yii\authclient\AuthAction;
+
+class NetworkController extends Controller
+{
+    private $service;
+
+    public function __construct($id, $module, NetworkService $service, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->service = $service;
+    }
+
+    public function actions()
+    {
+        return [
+            'auth' => [
+                'class' => AuthAction::class,
+                'successCallback' => [$this, 'onAuthSuccess'],
+            ],
+        ];
+    }
+
+    public function onAuthSuccess(ClientInterface $client): void
+    {
+        $network = $client->getId();
+        $attributes = $client->getUserAttributes();
+        $identity = ArrayHelper::getValue($attributes, 'id');
+
+        try {
+            $user = $this->service->auth($network, $identity);
+            Yii::$app->user->login($user, Yii::$app->params['user.rememberMeDuration']);
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+    }
+}
