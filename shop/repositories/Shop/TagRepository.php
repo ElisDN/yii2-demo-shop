5564932fe350992bfb87722ca67909eb43c75f30
ElisDN <mail@elisdn.ru>
2017-05-18 20:47:26 +0300

    Added tag

---
+++ shop/repositories/Shop/TagRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace shop\repositories\Shop;
+
+use shop\entities\Shop\Tag;
+use shop\repositories\NotFoundException;
+
+class TagRepository
+{
+    public function get($id): Tag
+    {
+        if (!$tag = Tag::findOne($id)) {
+            throw new NotFoundException('Tag is not found.');
+        }
+        return $tag;
+    }
+
+    public function save(Tag $tag): void
+    {
+        if (!$tag->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+
+    public function remove(Tag $tag): void
+    {
+        if (!$tag->delete()) {
+            throw new \RuntimeException('Removing error.');
+        }
+    }
+}
