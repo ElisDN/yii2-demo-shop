603b7d9467b51e4564731ecdea41a1b9bcdc22ad
ElisDN <mail@elisdn.ru>
2017-05-19 11:45:40 +0300

    Added category

---
+++ console/migrations/m170519_083418_create_shop_categories_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m170519_083418_create_shop_categories_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_categories}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull(),
+            'title' => $this->string(),
+            'description' => $this->string(),
+            'meta_json' => 'JSON NOT NULL',
+            'lft' => $this->integer()->notNull(),
+            'rgt' => $this->integer()->notNull(),
+            'depth' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_categories}}');
+    }
+}
