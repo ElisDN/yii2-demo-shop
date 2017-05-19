c99e542f8b0a4b989295332d7d3eabc6169cfab2
ElisDN <mail@elisdn.ru>
2017-05-19 18:50:44 +0300

    Added product reviews

---
+++ shop/services/manage/Shop/ReviewManageService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace shop\services;
+
+use shop\forms\manage\Shop\Product\ReviewEditForm;
+use shop\repositories\ProductRepository;
+
+class ReviewManageService
+{
+    private $products;
+
+    public function __construct(ProductRepository $products)
+    {
+        $this->products = $products;
+    }
+
+    public function edit($id, $reviewId, ReviewEditForm $form): void
+    {
+        $product = $this->products->get($id);
+        $product->editReview(
+            $reviewId,
+            $form->vote,
+            $form->text
+        );
+        $this->products->save($product);
+    }
+
+    public function activate($id, $reviewId): void
+    {
+        $product = $this->products->get($id);
+        $product->activateReview($reviewId);
+        $this->products->save($product);
+    }
+
+    public function draft($id, $reviewId): void
+    {
+        $product = $this->products->get($id);
+        $product->draftReview($reviewId);
+        $this->products->save($product);
+    }
+
+    public function remove($id, $reviewId): void
+    {
+        $product = $this->products->get($id);
+        $product->removeReview($reviewId);
+        $this->products->save($product);
+    }
+}
