fe47973e4a81957ebb974a9d51c3d50217b8ff3d
ElisDN <mail@elisdn.ru>
2017-05-21 20:04:45 +0300

    Added categories management

---
+++ backend/views/shop/category/view.php
@@ -0,0 +1,54 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $category shop\entities\Shop\Category */
+
+$this->title = $category->name;
+$this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-view">
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $category->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $category->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <div class="box">
+        <div class="box-header with-border">Common</div>
+        <div class="box-body">
+            <?= DetailView::widget([
+                'model' => $category,
+                'attributes' => [
+                    'id',
+                    'name',
+                    'slug',
+                    'title',
+                ],
+            ]) ?>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="box-header with-border">SEO</div>
+        <div class="box-body">
+            <?= DetailView::widget([
+                'model' => $category,
+                'attributes' => [
+                    'meta.title',
+                    'meta.description',
+                    'meta.keywords',
+                ],
+            ]) ?>
+        </div>
+    </div>
+</div>
