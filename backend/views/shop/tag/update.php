733983c832a2adf10d8c2ee47dedc66b2a45d2b4
ElisDN <mail@elisdn.ru>
2017-05-21 19:31:24 +0300

    Added tags management

---
+++ backend/views/shop/tag/update.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $tag shop\entities\Shop\Tag */
+/* @var $model shop\forms\manage\Shop\TagForm */
+
+$this->title = 'Update Tag: ' . $tag->name;
+$this->params['breadcrumbs'][] = ['label' => 'Tags', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $tag->name, 'url' => ['view', 'id' => $tag->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="tag-update">
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
