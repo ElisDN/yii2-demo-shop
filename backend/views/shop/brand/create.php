a5db264eb5010cfca491b7088f88f7f1f2a9299d
ElisDN <mail@elisdn.ru>
2017-05-21 19:26:49 +0300

    Added brands management

---
+++ backend/views/shop/brand/create.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $model shop\forms\manage\Shop\BrandForm */
+
+$this->title = 'Create Brand';
+$this->params['breadcrumbs'][] = ['label' => 'Brands', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="brand-create">
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
---
+++ backend/views/shop/brand/update.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $brand shop\entities\Shop\Brand */
+/* @var $model shop\forms\manage\Shop\BrandForm */
+
+$this->title = 'Update Brand: ' . $brand->name;
+$this->params['breadcrumbs'][] = ['label' => 'Brands', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $brand->name, 'url' => ['view', 'id' => $brand->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="brand-update">
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
