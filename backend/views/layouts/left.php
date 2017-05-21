4508c08f5014222ab0d0dc89530a495f73f3d248
ElisDN <mail@elisdn.ru>
2017-05-21 19:31:24 +0300

    Added tags management

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -33,6 +33,7 @@
                     ['label' => 'Management', 'options' => ['class' => 'header']],
                         ['label' => 'Shop', 'icon' => 'folder', 'items' => [
                             ['label' => 'Brands', 'icon' => 'file-o', 'url' => ['/shop/brand/index'], 'active' => $this->context->id == 'shop/brand'],
+                            ['label' => 'Tags', 'icon' => 'file-o', 'url' => ['/shop/tag/index'], 'active' => $this->context->id == 'shop/tag'],
                         ]],
                     ['label' => 'Users', 'icon' => 'user', 'url' => ['/user/index'], 'active' => $this->context->id == 'user/index'],
                 ],
