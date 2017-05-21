3f6410e60f04ecb20c4295eca92ba0fe8fb1aaee
Cyberha4 <proffi99@mail.ru>
2017-05-21 12:37:44 +0300

    fix return value in UserRepository

--- shop/repositories/UserRepository.php
+++ shop/repositories/UserRepository.php
@@ -36,7 +36,7 @@ class UserRepository
         return $this->getBy(['password_reset_token' => $token]);
     }
 
-    public function existsByPasswordResetToken(string $token): User
+    public function existsByPasswordResetToken(string $token): bool
     {
         return (bool) User::findByPasswordResetToken($token);
     }
