39048e6d422aac51b1b839acfc49d442a70bc8c5
ElisDN <mail@elisdn.ru>
2017-05-22 15:17:15 +0300

    Installed image upload behavior

--- shop/entities/Shop/Product/Photo.php
+++ shop/entities/Shop/Product/Photo.php
@@ -4,11 +4,14 @@ namespace shop\entities\Shop\Product;
 
 use yii\db\ActiveRecord;
 use yii\web\UploadedFile;
+use yiidreamteam\upload\ImageUploadBehavior;
 
 /**
  * @property integer $id
  * @property string $file
  * @property integer $sort
+ *
+ * @mixin ImageUploadBehavior
  */
 class Photo extends ActiveRecord
 {
@@ -32,5 +35,24 @@ class Photo extends ActiveRecord
     public static function tableName(): string
     {
         return '{{%shop_photos}}';
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            [
+                'class' => ImageUploadBehavior::className(),
+                'attribute' => 'file',
+                'createThumbsOnRequest' => true,
+                'filePath' => '@staticRoot/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'fileUrl' => '@static/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'thumbPath' => '@staticRoot/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                'thumbUrl' => '@static/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                'thumbs' => [
+                    'admin' => ['width' => 100, 'height' => 70],
+                    'thumb' => ['width' => 640, 'height' => 480],
+                ],
+            ],
+        ];
     }
 }
