f66ad46fca8e246b557ffc12357e998b1287f3fa
ElisDN <mail@elisdn.ru>
2017-05-22 14:03:24 +0300

    Moved categories into product edit form

--- shop/services/manage/Shop/ProductManageService.php
+++ shop/services/manage/Shop/ProductManageService.php
@@ -95,6 +95,7 @@ class ProductManageService
     {
         $product = $this->products->get($id);
         $brand = $this->brands->get($form->brandId);
+        $category = $this->categories->get($form->categories->main);
 
         $product->edit(
             $brand->id,
@@ -107,6 +108,15 @@ class ProductManageService
                 $form->meta->keywords
             )
         );
+
+        $product->changeMainCategory($category->id);
+
+        $product->revokeCategories();
+
+        foreach ($form->categories->others as $otherId) {
+            $category = $this->categories->get($otherId);
+            $product->assignCategory($category->id);
+        }
 
         foreach ($form->values as $value) {
             $product->setValue($value->id, $value->value);
@@ -129,20 +139,6 @@ class ProductManageService
             }
             $this->products->save($product);
         });
-    }
-
-
-    public function changeCategories($id, CategoriesForm $form): void
-    {
-        $product = $this->products->get($id);
-        $category = $this->categories->get($form->main);
-        $product->changeMainCategory($category->id);
-        $product->revokeCategories();
-        foreach ($form->others as $otherId) {
-            $category = $this->categories->get($otherId);
-            $product->assignCategory($category->id);
-        }
-        $this->products->save($product);
     }
 
     public function addPhotos($id, PhotosForm $form): void
