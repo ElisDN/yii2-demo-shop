66d76e84101a0160485a8e52cd6b2f8665e56d86
ElisDN <mail@elisdn.ru>
2017-05-22 13:00:18 +0300

    Added product description

--- shop/forms/manage/Shop/Product/ProductEditForm.php
+++ shop/forms/manage/Shop/Product/ProductEditForm.php
@@ -18,6 +18,7 @@ class ProductEditForm extends CompositeForm
     public $brandId;
     public $code;
     public $name;
+    public $description;
 
     private $_product;
 
@@ -26,6 +27,7 @@ class ProductEditForm extends CompositeForm
         $this->brandId = $product->brand_id;
         $this->code = $product->code;
         $this->name = $product->name;
+        $this->description = $product->description;
         $this->meta = new MetaForm($product->meta);
         $this->tags = new TagsForm($product);
         $this->values = array_map(function (Characteristic $characteristic) use ($product) {
@@ -42,6 +44,7 @@ class ProductEditForm extends CompositeForm
             [['brandId'], 'integer'],
             [['code', 'name'], 'string', 'max' => 255],
             [['code'], 'unique', 'targetClass' => Product::class, 'filter' => $this->_product ? ['<>', 'id', $this->_product->id] : null],
+            ['description', 'string'],
         ];
     }
 
