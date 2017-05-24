dae3a74515f71774ffeeae2a906dca981d6c5ec0
ElisDN <mail@elisdn.ru>
2017-05-24 16:28:30 +0300

    Added categories widget

--- shop/readModels/Shop/CategoryReadRepository.php
+++ shop/readModels/Shop/CategoryReadRepository.php
@@ -3,6 +3,7 @@
 namespace shop\readModels\Shop;
 
 use shop\entities\Shop\Category;
+use yii\helpers\ArrayHelper;
 
 class CategoryReadRepository
 {
@@ -14,5 +15,22 @@ class CategoryReadRepository
     public function find($id): ?Category
     {
         return Category::find()->andWhere(['id' => $id])->andWhere(['>', 'depth', 0])->one();
+    }
+
+    public function getTreeWithSubsOf(Category $category = null): array
+    {
+        $query = Category::find()->andWhere(['>', 'depth', 0])->orderBy('lft');
+
+        if ($category) {
+            $criteria = ['or', ['depth' => 1]];
+            foreach (ArrayHelper::merge([$category], $category->parents) as $item) {
+                $criteria[] = ['and', ['>', 'lft', $item->lft], ['<', 'rgt', $item->rgt], ['depth' => $item->depth + 1]];
+            }
+            $query->andWhere($criteria);
+        } else {
+            $query->andWhere(['depth' => 1]);
+        }
+
+        return $query->all();
     }
 }
