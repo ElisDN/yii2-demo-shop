22de2b036e229536de09656afc96fc13df72a73e
ElisDN <mail@elisdn.ru>
2017-05-24 12:43:42 +0300

    Added product status

--- backend/forms/Shop/ProductSearch.php
+++ backend/forms/Shop/ProductSearch.php
@@ -3,6 +3,7 @@
 namespace backend\forms\Shop;
 
 use shop\entities\Shop\Category;
+use shop\helpers\ProductHelper;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use shop\entities\Shop\Product\Product;
@@ -15,11 +16,12 @@ class ProductSearch extends Model
     public $name;
     public $category_id;
     public $brand_id;
+    public $status;
 
     public function rules(): array
     {
         return [
-            [['id', 'category_id', 'brand_id'], 'integer'],
+            [['id', 'category_id', 'brand_id', 'status'], 'integer'],
             [['code', 'name'], 'safe'],
         ];
     }
@@ -50,6 +52,7 @@ class ProductSearch extends Model
             'id' => $this->id,
             'category_id' => $this->category_id,
             'brand_id' => $this->brand_id,
+            'status' => $this->status,
         ]);
 
         $query
@@ -64,5 +67,10 @@ class ProductSearch extends Model
         return ArrayHelper::map(Category::find()->andWhere(['>', 'depth', 0])->orderBy('lft')->asArray()->all(), 'id', function (array $category) {
             return ($category['depth'] > 1 ? str_repeat('-- ', $category['depth'] - 1) . ' ' : '') . $category['name'];
         });
+    }
+
+    public function statusList(): array
+    {
+        return ProductHelper::statusList();
     }
 }
---
+++ console/migrations/m170524_085700_add_shop_product_status_field.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Migration;
+
+class m170524_085700_add_shop_product_status_field extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('{{%shop_products}}', 'status', $this->smallInteger()->notNull());
+        $this->update('{{%shop_products}}', ['status' => 1]);
+    }
+
+    public function down()
+    {
+        $this->dropColumn('{{%shop_products}}', 'status');
+    }
+}
