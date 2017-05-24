4ef82c7057b28673f7c96db14c7dac48cbf88921
ElisDN <mail@elisdn.ru>
2017-05-24 17:57:05 +0300

    Added SQL search

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -104,12 +104,14 @@ AppAsset::register($this);
                 </div>
             </div>
             <div class="col-sm-5">
+                <?= Html::beginForm(['/shop/catalog/search'], 'get') ?>
                 <div id="search" class="input-group">
-                    <input type="text" name="search" value="" placeholder="Search" class="form-control input-lg"/>
+                    <input type="text" name="text" value="" placeholder="Search" class="form-control input-lg"/>
                     <span class="input-group-btn">
-    <button type="button" class="btn btn-default btn-lg"><i class="fa fa-search"></i></button>
-  </span>
+                        <button type="submit" class="btn btn-default btn-lg"><i class="fa fa-search"></i></button>
+                    </span>
                 </div>
+                <?= Html::endForm() ?>
             </div>
             <div class="col-sm-3">
                 <div id="cart" class="btn-group btn-block">
