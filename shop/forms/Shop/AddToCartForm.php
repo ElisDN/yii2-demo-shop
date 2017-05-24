82918c64ac19cbf345662924d6b25d16c50fefa8
ElisDN <mail@elisdn.ru>
2017-05-24 14:53:02 +0300

    Added cart adding form

---
+++ shop/forms/Shop/AddToCartForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace shop\forms\Shop;
+
+use shop\entities\Shop\Product\Modification;
+use shop\entities\Shop\Product\Product;
+use shop\helpers\PriceHelper;
+use yii\base\Model;
+use yii\helpers\ArrayHelper;
+
+class AddToCartForm extends Model
+{
+    public $modification;
+    public $quantity;
+
+    private $_product;
+
+    public function __construct(Product $product, $config = [])
+    {
+        $this->_product = $product;
+        $this->quantity = 1;
+        parent::__construct($config);
+    }
+
+    public function rules(): array
+    {
+        return array_filter([
+            $this->_product->modifications ? ['modification', 'required'] : false,
+            ['quantity', 'required'],
+        ]);
+    }
+
+    public function modificationsList(): array
+    {
+        return ArrayHelper::map($this->_product->modifications, 'id', function (Modification $modification) {
+            return $modification->code . ' - ' . $modification->name . ' (' . PriceHelper::format($modification->price ?: $this->_product->price_new) . ')';
+        });
+    }
+}
