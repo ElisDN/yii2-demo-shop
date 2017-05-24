90c5a6f39cd1ef2380274f5f36e8f0f3324f3676
ElisDN <mail@elisdn.ru>
2017-05-24 15:29:51 +0300

    Added category breabcrumbs

--- frontend/views/shop/catalog/product.php
+++ frontend/views/shop/catalog/product.php
@@ -17,6 +17,12 @@ $this->registerMetaTag(['name' =>'description', 'content' => $product->meta->des
 $this->registerMetaTag(['name' =>'keywords', 'content' => $product->meta->keywords]);
 
 $this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+foreach ($product->category->parents as $parent) {
+    if (!$parent->isRoot()) {
+        $this->params['breadcrumbs'][] = ['label' => $parent->name, 'url' => ['category', 'id' => $parent->id]];
+    }
+}
+$this->params['breadcrumbs'][] = ['label' => $product->category->name, 'url' => ['category', 'id' => $product->category->id]];
 $this->params['breadcrumbs'][] = $product->name;
 
 MagnificPopupAsset::register($this);
