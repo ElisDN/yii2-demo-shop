bc5f7b5598240198e3d71d56e294accf0b9f2ca9
ElisDN <mail@elisdn.ru>
2017-05-24 18:20:21 +0300

    Added watermarks

--- shop/entities/Shop/Product/Photo.php
+++ shop/entities/Shop/Product/Photo.php
@@ -2,6 +2,7 @@
 
 namespace shop\entities\Shop\Product;
 
+use shop\services\WaterMarker;
 use yii\db\ActiveRecord;
 use yii\web\UploadedFile;
 use yiidreamteam\upload\ImageUploadBehavior;
@@ -52,8 +53,9 @@ class Photo extends ActiveRecord
                     'admin' => ['width' => 100, 'height' => 70],
                     'thumb' => ['width' => 640, 'height' => 480],
                     'catalog_list' => ['width' => 228, 'height' => 228],
-                    'catalog_product_main' => ['width' => 750, 'height' => 1000],
+                    'catalog_product_main' => ['processor' => [new WaterMarker(750, 1000, '@frontend/web/image/logo.png'), 'process']],
                     'catalog_product_additional' => ['width' => 66, 'height' => 66],
+                    'catalog_origin' => ['processor' => [new WaterMarker(1024, 768, '@frontend/web/image/logo.png'), 'process']],
                 ],
             ],
         ];
