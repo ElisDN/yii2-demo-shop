973a72e4cd3365c303bff911843a8d91e9017b4b
ElisDN <mail@elisdn.ru>
2017-05-24 16:28:30 +0300

    Added categories widget

--- frontend/views/shop/catalog/product.php
+++ frontend/views/shop/catalog/product.php
@@ -25,6 +25,8 @@ foreach ($product->category->parents as $parent) {
 $this->params['breadcrumbs'][] = ['label' => $product->category->name, 'url' => ['category', 'id' => $product->category->id]];
 $this->params['breadcrumbs'][] = $product->name;
 
+$this->params['active_category'] = $product->category;
+
 MagnificPopupAsset::register($this);
 ?>
 
