b907e0dc277e7f876d91ce3f45490da413c7ebe6
ElisDN <mail@elisdn.ru>
2017-05-25 15:43:30 +0300

    Added featured product widget

---
+++ frontend/widgets/FeaturedProductsWidget.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace frontend\widgets;
+
+use shop\readModels\Shop\ProductReadRepository;
+use yii\base\Widget;
+
+class FeaturedProductsWidget extends Widget
+{
+    public $limit;
+
+    private $repository;
+
+    public function __construct(ProductReadRepository $repository, $config = [])
+    {
+        parent::__construct($config);
+        $this->repository = $repository;
+    }
+
+    public function run()
+    {
+        return $this->render('featured', [
+            'products' => $this->repository->getFeatured($this->limit)
+        ]);
+    }
+}
