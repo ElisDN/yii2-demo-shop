518ff1cb55b5e9c5e8595759edfa1619044c30f2
ElisDN <mail@elisdn.ru>
2017-05-25 19:00:17 +0300

    Added ES index

--- shop/entities/Shop/Product/queries/ProductQuery.php
+++ shop/entities/Shop/Product/queries/ProductQuery.php
@@ -7,6 +7,10 @@ use yii\db\ActiveQuery;
 
 class ProductQuery extends ActiveQuery
 {
+    /**
+     * @param null $alias
+     * @return $this
+     */
     public function active($alias = null)
     {
         return $this->andWhere([
