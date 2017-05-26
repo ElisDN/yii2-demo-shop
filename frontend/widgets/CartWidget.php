9bb14dd8db2658be532d669552c9635133b055cf
ElisDN <mail@elisdn.ru>
2017-05-26 17:36:15 +0300

    Added cart widget

---
+++ frontend/widgets/CartWidget.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\widgets;
+
+use shop\cart\Cart;
+use yii\base\Widget;
+
+class CartWidget extends Widget
+{
+    private $cart;
+
+    public function __construct(Cart $cart, $config = [])
+    {
+        parent::__construct($config);
+        $this->cart = $cart;
+    }
+
+    public function run(): string
+    {
+        return $this->render('cart', [
+            'cart' => $this->cart,
+        ]);
+    }
+}
