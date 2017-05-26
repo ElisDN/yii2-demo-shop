028f6c44e21f563ecf43efde11a0546a97173b06
ElisDN <mail@elisdn.ru>
2017-05-26 12:37:13 +0300

    Added ES search

--- shop/readModels/Shop/ProductReadRepository.php
+++ shop/readModels/Shop/ProductReadRepository.php
@@ -2,19 +2,30 @@
 
 namespace shop\readModels\Shop;
 
+use Elasticsearch\Client;
 use shop\entities\Shop\Brand;
 use shop\entities\Shop\Category;
 use shop\entities\Shop\Product\Product;
-use shop\entities\Shop\Product\Value;
 use shop\entities\Shop\Tag;
 use shop\forms\Shop\Search\SearchForm;
+use shop\forms\Shop\Search\ValueForm;
 use yii\data\ActiveDataProvider;
 use yii\data\DataProviderInterface;
+use yii\data\Pagination;
+use yii\data\Sort;
 use yii\db\ActiveQuery;
+use yii\db\Expression;
 use yii\helpers\ArrayHelper;
 
 class ProductReadRepository
 {
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
     public function getAll(): DataProviderInterface
     {
         $query = Product::find()->alias('p')->active('p')->with('mainPhoto');
@@ -90,66 +101,80 @@ class ProductReadRepository
 
     public function search(SearchForm $form): DataProviderInterface
     {
-        $query = Product::find()->alias('p')->active('p')->with('mainPhoto', 'category');
+        $pagination = new Pagination([
+            'pageSizeLimit' => [15, 100],
+            'validatePage' => false,
+        ]);
 
-        if ($form->brand) {
-            $query->andWhere(['p.brand_id' => $form->brand]);
-        }
+        $sort = new Sort([
+            'defaultOrder' => ['id' => SORT_DESC],
+            'attributes' => [
+                'id',
+                'name',
+                'price',
+                'rating',
+            ],
+        ]);
 
-        if ($form->category) {
-            if ($category = Category::findOne($form->category)) {
-                $ids = ArrayHelper::merge([$form->category], $category->getChildren()->select('id')->column());
-                $query->joinWith(['categoryAssignments ca'], false);
-                $query->andWhere(['or', ['p.category_id' => $ids], ['ca.category_id' => $ids]]);
-            } else {
-                $query->andWhere(['p.id' => 0]);
-            }
-        }
-
-        if ($form->values) {
-            $productIds = null;
-            foreach ($form->values as $value) {
-                if ($value->isFilled()) {
-                    $q = Value::find()->andWhere(['characteristic_id' => $value->getId()]);
-
-                    $q->andFilterWhere(['>=', 'CAST(value AS SIGNED)', $value->from]);
-                    $q->andFilterWhere(['<=', 'CAST(value AS SIGNED)', $value->to]);
-                    $q->andFilterWhere(['value' => $value->equal]);
-
-                    $foundIds = $q->select('product_id')->column();
-                    $productIds = $productIds === null ? $foundIds : array_intersect($productIds, $foundIds);
-                }
-            }
-            if ($productIds !== null) {
-                $query->andWhere(['p.id' => $productIds]);
-            }
-        }
-
-        if (!empty($form->text)) {
-            $query->andWhere(['or', ['like', 'code', $form->text], ['like', 'name', $form->text]]);
-        }
-
-        $query->groupBy('p.id');
-
-        return new ActiveDataProvider([
-            'query' => $query,
-            'sort' => [
-                'defaultOrder' => ['id' => SORT_DESC],
-                'attributes' => [
-                    'id' => [
-                        'asc' => ['p.id' => SORT_ASC],
-                        'desc' => ['p.id' => SORT_DESC],
-                    ],
-                    'name' => [
-                        'asc' => ['p.name' => SORT_ASC],
-                        'desc' => ['p.name' => SORT_DESC],
-                    ],
-                    'price' => [
-                        'asc' => ['p.price_new' => SORT_ASC],
-                        'desc' => ['p.price_new' => SORT_DESC],
+        $response = $this->client->search([
+            'index' => 'shop',
+            'type' => 'products',
+            'body' => [
+                '_source' => ['id'],
+                'from' => $pagination->getOffset(),
+                'size' => $pagination->getLimit(),
+                'sort' => array_map(function ($attribute, $direction) {
+                    return [$attribute => ['order' => $direction === SORT_ASC ? 'asc' : 'desc']];
+                }, array_keys($sort->getOrders()), $sort->getOrders()),
+                'query' => [
+                    'bool' => [
+                        'must' => array_merge(
+                            array_filter([
+                                !empty($form->category) ? ['term' => ['categories' => $form->category]] : false,
+                                !empty($form->brand) ? ['term' => ['brand' => $form->brand]] : false,
+                                !empty($form->text) ? ['multi_match' => [
+                                    'query' => $form->text,
+                                    'fields' => [ 'name^3', 'description' ]
+                                ]] : false,
+                            ]),
+                            array_map(function (ValueForm $value) {
+                                return ['nested' => [
+                                    'path' => 'values',
+                                    'query' => [
+                                        'bool' => [
+                                            'must' => array_filter([
+                                                ['match' => ['values.characteristic' => $value->getId()]],
+                                                !empty($value->equal) ? ['match' => ['values.value_string' => $value->equal]] : false,
+                                                !empty($value->from) ? ['range' => ['values.value_int' => ['gte' => $value->from]]] : false,
+                                                !empty($value->to) ? ['range' => ['values.value_int' => ['lte' => $value->to]]] : false,
+                                            ]),
+                                        ],
+                                    ],
+                                ]];
+                            }, array_filter($form->values, function (ValueForm $value) { return $value->isFilled(); }))
+                        )
                     ],
                 ],
-            ]
+            ],
+        ]);
+
+        $ids = ArrayHelper::getColumn($response['hits']['hits'], '_source.id');
+
+        if ($ids) {
+            $query = Product::find()
+                ->active()
+                ->with('mainPhoto')
+                ->andWhere(['id' => $ids])
+                ->orderBy(new Expression('FIELD(id,' . implode(',', $ids) . ')'));
+        } else {
+            $query = Product::find()->andWhere(['id' => 0]);
+        }
+
+        return new SimpleActiveDataProvider([
+            'query' => $query,
+            'totalCount' => $response['hits']['total'],
+            'pagination' => $pagination,
+            'sort' => $sort,
         ]);
     }
 }
