1132316e4e4555d4698e4d76a197ce94b3dcec8c
ElisDN <mail@elisdn.ru>
2017-05-29 18:18:05 +0300

    Added cabinet orders page

---
+++ frontend/views/cabinet/order/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use shop\entities\Shop\Order\Order;
+use shop\helpers\OrderHelper;
+use yii\grid\ActionColumn;
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Orders';
+$this->params['breadcrumbs'][] = ['label' => 'Cabinet', 'url' => ['cabinet/default/index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <div class="box">
+        <div class="box-body">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => [
+                    [
+                        'attribute' => 'id',
+                        'value' => function (Order $model) {
+                            return Html::a(Html::encode($model->id), ['view', 'id' => $model->id]);
+                        },
+                        'format' => 'raw',
+                    ],
+                    'created_at:datetime',
+                    [
+                        'attribute' => 'status',
+                        'value' => function (Order $model) {
+                            return OrderHelper::statusLabel($model->current_status);
+                        },
+                        'format' => 'raw',
+                    ],
+                    ['class' => ActionColumn::class],
+                ],
+            ]); ?>
+        </div>
+    </div>
+</div>
