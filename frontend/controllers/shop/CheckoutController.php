a8f2576470a5fe8c3750c09e53ea35fbbfecc145
ElisDN <mail@elisdn.ru>
2017-05-29 18:16:59 +0300

    Added checkout page

---
+++ frontend/controllers/shop/CheckoutController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace frontend\controllers\shop;
+
+use shop\cart\Cart;
+use shop\forms\Shop\Order\OrderForm;
+use shop\services\Shop\OrderService;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+class CheckoutController extends Controller
+{
+    public $layout = 'blank';
+
+    private $service;
+    private $cart;
+
+    public function __construct($id, $module, OrderService $service, Cart $cart, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->service = $service;
+        $this->cart = $cart;
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $form = new OrderForm($this->cart->getWeight());
+
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $order = $this->service->checkout(Yii::$app->user->id, $form);
+                return $this->redirect(['/cabinet/order/view', 'id' => $order->id]);
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('index', [
+            'cart' => $this->cart,
+            'model' => $form,
+        ]);
+    }
+}
