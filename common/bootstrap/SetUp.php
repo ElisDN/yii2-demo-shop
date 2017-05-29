27318187ee4e34160b8f37dd9fbee29a65cef526
ElisDN <mail@elisdn.ru>
2017-05-29 17:03:43 +0300

    Added cookie cart storage

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -7,6 +7,7 @@ use Elasticsearch\ClientBuilder;
 use shop\cart\Cart;
 use shop\cart\cost\calculator\DynamicCost;
 use shop\cart\cost\calculator\SimpleCost;
+use shop\cart\storage\CookieStorage;
 use shop\cart\storage\SessionStorage;
 use shop\services\ContactService;
 use yii\base\BootstrapInterface;
@@ -37,7 +38,7 @@ class SetUp implements BootstrapInterface
 
         $container->setSingleton(Cart::class, function () {
             return new Cart(
-                new SessionStorage('cart', \Yii::$app->session),
+                new CookieStorage('cart', 3600),
                 new DynamicCost(new SimpleCost())
             );
         });
