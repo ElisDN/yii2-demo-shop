228c454a53244615453a83a53955a5327ea654a0
ElisDN <mail@elisdn.ru>
2017-05-29 18:18:05 +0300

    Added cabinet orders page

---
+++ shop/readModels/Shop/OrderReadRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace shop\readModels\Shop;
+
+use shop\entities\Shop\Order\Order;
+use yii\data\ActiveDataProvider;
+
+class OrderReadRepository
+{
+    public function getOwm($userId): ActiveDataProvider
+    {
+        return new ActiveDataProvider([
+            'query' => Order::find()
+                ->andWhere(['user_id' => $userId])
+                ->orderBy(['id' => SORT_DESC]),
+            'sort' => false,
+        ]);
+    }
+
+    public function findOwn($userId, $id): ?Order
+    {
+        return Order::find()->andWhere(['user_id' => $userId, 'id' => $id])->one();
+    }
+}
