2d13a4f485f5c084cb2dd6e4c5e622664feb5fbd
ElisDN <mail@elisdn.ru>
2017-05-29 18:18:05 +0300

    Added cabinet orders page

---
+++ frontend/views/cabinet/order/view.php
@@ -0,0 +1,70 @@
+<?php
+
+use shop\helpers\OrderHelper;
+use shop\helpers\PriceHelper;
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $order shop\entities\Shop\Order\Order */
+
+$this->title = 'Order ' . $order->id;
+$this->params['breadcrumbs'][] = ['label' => 'Cabinet', 'url' => ['cabinet/default/index']];
+$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= DetailView::widget([
+        'model' => $order,
+        'attributes' => [
+            'id',
+            'created_at:datetime',
+            [
+                'attribute' => 'current_status',
+                'value' => OrderHelper::statusLabel($order->current_status),
+                'format' => 'raw',
+            ],
+            'delivery_method_name',
+            'deliveryData.index',
+            'deliveryData.address',
+            'cost',
+            'note:ntext',
+        ],
+    ]) ?>
+
+    <div class="table-responsive">
+        <table class="table table-bordered" style="margin-bottom: 0">
+            <thead>
+            <tr>
+                <th class="text-left">Product Name</th>
+                <th class="text-left">Model</th>
+                <th class="text-left">Quantity</th>
+                <th class="text-right">Unit Price</th>
+                <th class="text-right">Total</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($order->items as $item): ?>
+                <tr>
+                    <td class="text-left">
+                        <?= Html::encode($item->product_name) ?>
+                    </td>
+                    <td class="text-left">
+                        <?= Html::encode($item->modification_code) ?>
+                        <?= Html::encode($item->modification_name) ?>
+                    </td>
+                    <td class="text-left">
+                        <?= $item->quantity ?>
+                    </td>
+                    <td class="text-right"><?= PriceHelper::format($item->price) ?></td>
+                    <td class="text-right"><?= PriceHelper::format($item->getCost()) ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+</div>
