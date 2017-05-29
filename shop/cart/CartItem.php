8768abb276fe4fdfacd63a40022e49c086daa9f8
ElisDN <mail@elisdn.ru>
2017-05-29 17:03:43 +0300

    Added cookie cart storage

--- shop/cart/CartItem.php
+++ shop/cart/CartItem.php
@@ -26,12 +26,17 @@ class CartItem
         return md5(serialize([$this->product->id, $this->modificationId]));
     }
 
+    public function getProductId(): int
+    {
+        return $this->product->id;
+    }
+
     public function getProduct(): Product
     {
         return $this->product;
     }
 
-    public function getModificationId(): ?Modification
+    public function getModificationId(): ?int
     {
         return $this->modificationId;
     }
