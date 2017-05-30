c4c6fefb24032529b335c62eb3bfdba20c429be1
ElisDN <mail@elisdn.ru>
2017-05-30 20:32:19 +0300

    Added blog categories

---
+++ backend/forms/Blog/CategorySearch.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace backend\forms\Blog;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use shop\entities\Blog\Category;
+
+class CategorySearch extends Model
+{
+    public $id;
+    public $name;
+    public $slug;
+    public $title;
+
+    public function rules(): array
+    {
+        return [
+            [['id'], 'integer'],
+            [['name', 'slug', 'title'], 'safe'],
+        ];
+    }
+
+    /**
+     * @param array $params
+     * @return ActiveDataProvider
+     */
+    public function search(array $params): ActiveDataProvider
+    {
+        $query = Category::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort' => [
+                'defaultOrder' => ['sort' => SORT_ASC]
+            ]
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query
+            ->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'slug', $this->slug])
+            ->andFilterWhere(['like', 'title', $this->title]);
+
+        return $dataProvider;
+    }
+}
