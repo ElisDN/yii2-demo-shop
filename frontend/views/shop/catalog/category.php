b9be0602c7c6f49a56e7fa9c97b095e7282e778e
ElisDN <mail@elisdn.ru>
2017-05-31 17:12:06 +0300

    Added CKEditor

--- frontend/views/shop/catalog/category.php
+++ frontend/views/shop/catalog/category.php
@@ -31,7 +31,13 @@ $this->params['active_category'] = $category;
 <?php if (trim($category->description)): ?>
     <div class="panel panel-default">
         <div class="panel-body">
-            <?= Yii::$app->formatter->asNtext($category->description) ?>
+            <?= Yii::$app->formatter->asHtml($category->description, [
+                'Attr.AllowedRel' => array('nofollow'),
+                'HTML.SafeObject' => true,
+                'Output.FlashCompat' => true,
+                'HTML.SafeIframe' => true,
+                'URI.SafeIframeRegexp'=>'%^(https?:)?//(www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%',
+            ]) ?>
         </div>
     </div>
 <?php endif; ?>
