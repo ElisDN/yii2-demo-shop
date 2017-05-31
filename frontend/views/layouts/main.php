32b2d01484d65c6baaa84af2335abfdb4db9cfcc
ElisDN <mail@elisdn.ru>
2017-05-31 12:42:06 +0300

    Moved shop widgets

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -5,7 +5,7 @@
 
 use common\widgets\Alert;
 use frontend\assets\AppAsset;
-use frontend\widgets\CartWidget;
+use frontend\widgets\Shop\CartWidget;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\helpers\Html;
