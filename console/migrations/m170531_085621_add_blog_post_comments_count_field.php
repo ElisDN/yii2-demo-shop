7c210dde43d0e7ad9b58b078eaa6dd14b48cc7c6
ElisDN <mail@elisdn.ru>
2017-05-31 12:36:26 +0300

    Added blog post comments

---
+++ console/migrations/m170531_085621_add_blog_post_comments_count_field.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m170531_085621_add_blog_post_comments_count_field extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('{{%blog_posts}}', 'comments_count', $this->integer()->notNull());
+
+        $this->update('{{%blog_posts}}', ['comments_count' => 0]);
+    }
+
+    public function down()
+    {
+        $this->dropColumn('{{%blog_posts}}', 'comments_count');
+    }
+}
---
+++ shop/forms/Blog/CommentForm.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace shop\forms\Blog;
+
+use yii\base\Model;
+
+class CommentForm extends Model
+{
+    public $parentId;
+    public $text;
+
+    public function rules(): array
+    {
+        return [
+            [['text'], 'required'],
+            ['text', 'string'],
+            ['parentId', 'integer'],
+        ];
+    }
+}
