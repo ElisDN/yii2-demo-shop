2a4f824d3205640a14c65e4396fbb7d9a64c5fc2
ElisDN <mail@elisdn.ru>
2017-05-31 16:53:45 +0300

    Added pages backend

---
+++ backend/views/page/view.php
@@ -0,0 +1,60 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $page shop\entities\Page */
+
+$this->title = $page->title;
+$this->params['breadcrumbs'][] = ['label' => 'Pages', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-view">
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $page->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $page->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <div class="box">
+        <div class="box-header with-border">Common</div>
+        <div class="box-body">
+            <?= DetailView::widget([
+                'model' => $page,
+                'attributes' => [
+                    'id',
+                    'title',
+                    'slug',
+                ],
+            ]) ?>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="box-header with-border">Content</div>
+        <div class="box-body">
+            <?= Yii::$app->formatter->asNtext($page->content) ?>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="box-header with-border">SEO</div>
+        <div class="box-body">
+            <?= DetailView::widget([
+                'model' => $page,
+                'attributes' => [
+                    'meta.title',
+                    'meta.description',
+                    'meta.keywords',
+                ],
+            ]) ?>
+        </div>
+    </div>
+</div>
