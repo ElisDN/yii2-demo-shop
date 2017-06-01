1a27786ca2b64a7ee4c84f8c39b41fce38a17b63
ElisDN <mail@elisdn.ru>
2017-06-01 20:56:26 +0300

    Extracted user identity

--- common/config/test.php
+++ common/config/test.php
@@ -7,7 +7,7 @@ return [
     'components' => [
         'user' => [
             'class' => 'yii\web\User',
-            'identityClass' => 'shop\entities\User\User',
+            'identityClass' => 'common\auth\Identity',
             'identityCookie' => new ReplaceArrayValue(['name' => '_identity', 'httpOnly' => true]),
         ],
     ],
