b323990c9308c048afabf4b7afd0b6936c5f25d1
ElisDN <mail@elisdn.ru>
2017-06-01 21:05:56 +0300

    Added OAuth2 module

--- common/auth/Identity.php
+++ common/auth/Identity.php
@@ -2,12 +2,14 @@
 
 namespace common\auth;
 
+use filsh\yii2\oauth2server\Module;
+use OAuth2\Storage\UserCredentialsInterface;
 use shop\entities\User\User;
 use shop\readModels\UserReadRepository;
-use yii\base\NotSupportedException;
+use Yii;
 use yii\web\IdentityInterface;
 
-class Identity implements IdentityInterface
+class Identity implements IdentityInterface, UserCredentialsInterface
 {
     private $user;
 
@@ -24,7 +26,8 @@ class Identity implements IdentityInterface
 
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+        $data = self::getOauth()->getServer()->getResourceController()->getToken();
+        return !empty($data['user_id']) ? static::findIdentity($data['user_id']) : null;
     }
 
     public function getId(): int
@@ -42,8 +45,27 @@ class Identity implements IdentityInterface
         return $this->getAuthKey() === $authKey;
     }
 
+    public function checkUserCredentials($username, $password): bool
+    {
+        if (!$user = self::getRepository()->findActiveByUsername($username)) {
+            return false;
+        }
+        return $user->validatePassword($password);
+    }
+
+    public function getUserDetails($username): array
+    {
+        $user = self::getRepository()->findActiveByUsername($username);
+        return ['user_id' => $user->id];
+    }
+
     private static function getRepository(): UserReadRepository
     {
         return \Yii::$container->get(UserReadRepository::class);
+    }
+
+    private static function getOauth(): Module
+    {
+        return Yii::$app->getModule('oauth2');
     }
 }
