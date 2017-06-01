b711703e040f948add14e6861309b2b4643b0a16
ElisDN <mail@elisdn.ru>
2017-06-01 21:05:56 +0300

    Added OAuth2 module

--- shop/readModels/UserReadRepository.php
+++ shop/readModels/UserReadRepository.php
@@ -11,6 +11,11 @@ class UserReadRepository
         return User::findOne($id);
     }
 
+    public function findActiveByUsername($username): ?User
+    {
+        return User::findOne(['username' => $username, 'status' => User::STATUS_ACTIVE]);
+    }
+
     public function findActiveById($id): ?User
     {
         return User::findOne(['id' => $id, 'status' => User::STATUS_ACTIVE]);
