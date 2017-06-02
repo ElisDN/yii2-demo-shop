365e0a4f88a8f5f1565d8ac7fdec0216772417c5
ElisDN <mail@elisdn.ru>
2017-06-02 17:56:26 +0300

    Added Swagger annotations

--- api/controllers/shop/ProductController.php
+++ api/controllers/shop/ProductController.php
@@ -52,12 +52,43 @@ class ProductController extends Controller
         ];
     }
 
+    /**
+     * @SWG\Get(
+     *     path="/shop/products",
+     *     tags={"Catalog"},
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Success response",
+     *         @SWG\Schema(
+     *             type="array",
+     *             @SWG\Items(ref="#/definitions/ProductItem")
+     *         ),
+     *     ),
+     *     security={{"Bearer": {}, "OAuth2": {}}}
+     * )
+     */
     public function actionIndex(): DataProviderInterface
     {
         $dataProvider = $this->products->getAll();
         return new MapDataProvider($dataProvider, [$this, 'serializeListItem']);
     }
 
+    /**
+     * @SWG\Get(
+     *     path="/shop/products/category/{categoryId}",
+     *     tags={"Catalog"},
+     *     @SWG\Parameter(name="categoryId", in="path", required=true, type="integer"),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Success response",
+     *         @SWG\Schema(ref="#/definitions/ProductItem")
+     *     ),
+     *     security={{"Bearer": {}, "OAuth2": {}}}
+     * )
+     * @param $id
+     * @return DataProviderInterface
+     * @throws NotFoundHttpException
+     */
     public function actionCategory($id): DataProviderInterface
     {
         if (!$category = $this->categories->find($id)) {
@@ -67,6 +98,22 @@ class ProductController extends Controller
         return new MapDataProvider($dataProvider, [$this, 'serializeListItem']);
     }
 
+    /**
+     * @SWG\Get(
+     *     path="/shop/products/brand/{brandId}",
+     *     tags={"Catalog"},
+     *     @SWG\Parameter(name="brandId", in="path", required=true, type="integer"),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Success response",
+     *         @SWG\Schema(ref="#/definitions/ProductItem")
+     *     ),
+     *     security={{"Bearer": {}, "OAuth2": {}}}
+     * )
+     * @param $id
+     * @return DataProviderInterface
+     * @throws NotFoundHttpException
+     */
     public function actionBrand($id): DataProviderInterface
     {
         if (!$brand = $this->brands->find($id)) {
@@ -76,6 +123,22 @@ class ProductController extends Controller
         return new MapDataProvider($dataProvider, [$this, 'serializeListItem']);
     }
 
+    /**
+     * @SWG\Get(
+     *     path="/shop/products/tag/{tagId}",
+     *     tags={"Catalog"},
+     *     @SWG\Parameter(name="tagId", in="path", required=true, type="integer"),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Success response",
+     *         @SWG\Schema(ref="#/definitions/ProductItem")
+     *     ),
+     *     security={{"Bearer": {}, "OAuth2": {}}}
+     * )
+     * @param $id
+     * @return DataProviderInterface
+     * @throws NotFoundHttpException
+     */
     public function actionTag($id): DataProviderInterface
     {
         if (!$tag = $this->tags->find($id)) {
@@ -85,6 +148,29 @@ class ProductController extends Controller
         return new MapDataProvider($dataProvider, [$this, 'serializeListItem']);
     }
 
+    /**
+     * @SWG\Get(
+     *     path="/shop/products/{productId}",
+     *     tags={"Catalog"},
+     *     @SWG\Parameter(
+     *         name="productId",
+     *         description="ID of product",
+     *         in="path",
+     *         required=true,
+     *         type="integer"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Success response",
+     *         @SWG\Schema(ref="#/definitions/ProductView")
+     *     ),
+     *     security={{"Bearer": {}, "OAuth2": {}}}
+     * )
+     * 
+     * @param $id
+     * @return array
+     * @throws NotFoundHttpException
+     */
     public function actionView($id): array
     {
         if (!$product = $this->products->find($id)) {
@@ -196,3 +282,83 @@ class ProductController extends Controller
         ];
     }
 }
+
+/**
+ * @SWG\Definition(
+ *     definition="ProductItem",
+ *     type="object",
+ *     @SWG\Property(property="id", type="integer"),
+ *     @SWG\Property(property="code", type="string"),
+ *     @SWG\Property(property="name", type="string"),
+ *     @SWG\Property(property="category", ref="#/definitions/ProductCategory"),
+ *     @SWG\Property(property="brand", ref="#/definitions/ProductBrand"),
+ *     @SWG\Property(property="price", ref="#/definitions/ProductPrice"),
+ *     @SWG\Property(property="thumbnail", type="string"),
+ *     @SWG\Property(property="_links", type="object",
+ *         @SWG\Property(property="self", type="object", @SWG\Property(property="href", type="string")),
+ *     ),
+ * )
+ *
+ * @SWG\Definition(
+ *     definition="ProductView",
+ *     type="object",
+ *     @SWG\Property(property="id", type="integer"),
+ *     @SWG\Property(property="code", type="string"),
+ *     @SWG\Property(property="name", type="string"),
+ *     @SWG\Property(property="description", type="string"),
+ *     @SWG\Property(property="categories", type="object",
+ *         @SWG\Property(property="main", ref="#/definitions/ProductCategory"),
+ *         @SWG\Property(property="other", type="array", @SWG\Items(ref="#/definitions/ProductCategory")),
+ *     ),
+ *     @SWG\Property(property="brand", ref="#/definitions/ProductBrand"),
+ *     @SWG\Property(property="tags", type="array", @SWG\Items(ref="#/definitions/ProductTag")),
+ *     @SWG\Property(property="photos", type="array", @SWG\Items(ref="#/definitions/ProductPhoto")),
+ *     @SWG\Property(property="_links", type="object",
+ *         @SWG\Property(property="self", type="object", @SWG\Property(property="href", type="string")),
+ *     ),
+ * )
+ *
+ * @SWG\Definition(
+ *     definition="ProductCategory",
+ *     type="object",
+ *     @SWG\Property(property="id", type="integer"),
+ *     @SWG\Property(property="name", type="string"),
+ *     @SWG\Property(property="_links", type="object",
+ *         @SWG\Property(property="self", type="object", @SWG\Property(property="href", type="string")),
+ *     ),
+ * )
+ *
+ * @SWG\Definition(
+ *     definition="ProductBrand",
+ *     type="object",
+ *     @SWG\Property(property="id", type="integer"),
+ *     @SWG\Property(property="name", type="string"),
+ *     @SWG\Property(property="_links", type="object",
+ *         @SWG\Property(property="self", type="object", @SWG\Property(property="href", type="string")),
+ *     ),
+ * )
+ *
+ * @SWG\Definition(
+ *     definition="ProductTag",
+ *     type="object",
+ *     @SWG\Property(property="id", type="integer"),
+ *     @SWG\Property(property="name", type="string"),
+ *     @SWG\Property(property="_links", type="object",
+ *         @SWG\Property(property="self", type="object", @SWG\Property(property="href", type="string")),
+ *     ),
+ * )
+ *
+ * @SWG\Definition(
+ *     definition="ProductPrice",
+ *     type="object",
+ *     @SWG\Property(property="new", type="integer"),
+ *     @SWG\Property(property="old", type="integer"),
+ * )
+ *
+ * @SWG\Definition(
+ *     definition="ProductPhoto",
+ *     type="object",
+ *     @SWG\Property(property="thumbnail", type="string"),
+ *     @SWG\Property(property="origin", type="string"),
+ * )
+ */
