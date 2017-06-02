667854c05754f9566d14200893efafb54f36963a
ElisDN <mail@elisdn.ru>
2017-06-02 11:46:32 +0300

    Added RBAC

--- common/config/main.php
+++ common/config/main.php
@@ -10,5 +10,12 @@ return [
             'class' => 'yii\caching\FileCache',
             'cachePath' => '@common/runtime/cache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'itemTable' => '{{%auth_items}}',
+            'itemChildTable' => '{{%auth_item_children}}',
+            'assignmentTable' => '{{%auth_assignments}}',
+            'ruleTable' => '{{%auth_rules}}',
+        ],
     ],
 ];
--- shop/forms/manage/User/UserCreateForm.php
+++ shop/forms/manage/User/UserCreateForm.php
@@ -4,21 +4,28 @@ namespace shop\forms\manage\User;
 
 use shop\entities\User\User;
 use yii\base\Model;
+use yii\helpers\ArrayHelper;
 
 class UserCreateForm extends Model
 {
     public $username;
     public $email;
     public $password;
+    public $role;
 
     public function rules(): array
     {
         return [
-            [['username', 'email'], 'required'],
+            [['username', 'email', 'role'], 'required'],
             ['email', 'email'],
             [['username', 'email'], 'string', 'max' => 255],
             [['username', 'email'], 'unique', 'targetClass' => User::class],
             ['password', 'string', 'min' => 6],
         ];
+    }
+
+    public function rolesList(): array
+    {
+        return ArrayHelper::map(\Yii::$app->authManager->getRoles(), 'name', 'description');
     }
 }
