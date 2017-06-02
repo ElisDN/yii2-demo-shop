fdfbd8659848f237178883220319f7ea7f3124c3
ElisDN <mail@elisdn.ru>
2017-06-02 17:56:26 +0300

    Added Swagger annotations

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -4,8 +4,62 @@ namespace api\controllers;
 
 use yii\rest\Controller;
 
+/**
+ * @SWG\Swagger(
+ *     basePath="/",
+ *     host="api.shop.dev",
+ *     schemes={"http"},
+ *     produces={"application/json","application/xml"},
+ *     consumes={"application/json","application/xml"},
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Shop API",
+ *         description="HTTP JSON API",
+ *     ),
+ *     @SWG\SecurityScheme(
+ *         securityDefinition="OAuth2",
+ *         type="oauth2",
+ *         flow="password",
+ *         tokenUrl="http://api.shop.dev/oauth2/token"
+ *     ),
+ *     @SWG\SecurityScheme(
+ *         securityDefinition="Bearer",
+ *         type="apiKey",
+ *         name="Authorization",
+ *         in="header"
+ *     ),
+ *     @SWG\Definition(
+ *         definition="ErrorModel",
+ *         type="object",
+ *         required={"code", "message"},
+ *         @SWG\Property(
+ *             property="code",
+ *             type="integer",
+ *         ),
+ *         @SWG\Property(
+ *             property="message",
+ *             type="string"
+ *         )
+ *     )
+ * )
+ */
+
 class SiteController extends Controller
 {
+    /**
+     * @SWG\Get(
+     *     path="/",
+     *     tags={"Info"},
+     *     @SWG\Response(
+     *         response="200",
+     *         description="API version",
+     *         @SWG\Schema(
+     *             type="object",
+     *             @SWG\Property(property="version", type="string")
+     *         ),
+     *     )
+     * )
+     */
     public function actionIndex(): array
     {
         return [
