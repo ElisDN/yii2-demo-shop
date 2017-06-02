aacbb886dd5e19b125ad3114c29c915c701f85ab
ElisDN <mail@elisdn.ru>
2017-06-02 11:46:32 +0300

    Added RBAC

--- shop/forms/manage/User/UserEditForm.php
+++ shop/forms/manage/User/UserEditForm.php
@@ -3,12 +3,15 @@
 namespace shop\forms\manage\User;
 
 use shop\entities\User\User;
+use Yii;
 use yii\base\Model;
+use yii\helpers\ArrayHelper;
 
 class UserEditForm extends Model
 {
     public $username;
     public $email;
+    public $role;
 
     public $_user;
 
@@ -16,6 +19,8 @@ class UserEditForm extends Model
     {
         $this->username = $user->username;
         $this->email = $user->email;
+        $roles = Yii::$app->authManager->getRolesByUser($user->id);
+        $this->role = $roles ? reset($roles)->name : null;
         $this->_user = $user;
         parent::__construct($config);
     }
@@ -23,10 +28,15 @@ class UserEditForm extends Model
     public function rules(): array
     {
         return [
-            [['username', 'email'], 'required'],
+            [['username', 'email', 'role'], 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
             [['username', 'email'], 'unique', 'targetClass' => User::class, 'filter' => ['<>', 'id', $this->_user->id]],
         ];
+    }
+
+    public function rolesList(): array
+    {
+        return ArrayHelper::map(Yii::$app->authManager->getRoles(), 'name', 'description');
     }
 }
--- shop/services/manage/UserManageService.php
+++ shop/services/manage/UserManageService.php
@@ -6,14 +6,20 @@ use shop\entities\User\User;
 use shop\forms\manage\User\UserCreateForm;
 use shop\forms\manage\User\UserEditForm;
 use shop\repositories\UserRepository;
+use shop\services\RoleManager;
+use shop\services\TransactionManager;
 
 class UserManageService
 {
     private $repository;
+    private $roles;
+    private $transaction;
 
-    public function __construct(UserRepository $repository)
+    public function __construct(UserRepository $repository, RoleManager $roles, TransactionManager $transaction)
     {
         $this->repository = $repository;
+        $this->roles = $roles;
+        $this->transaction = $transaction;
     }
 
     public function create(UserCreateForm $form): User
@@ -23,7 +29,10 @@ class UserManageService
             $form->email,
             $form->password
         );
-        $this->repository->save($user);
+        $this->transaction->wrap(function () use ($user, $form) {
+            $this->repository->save($user);
+            $this->roles->assign($user->id, $form->role);
+        });
         return $user;
     }
 
@@ -34,7 +43,10 @@ class UserManageService
             $form->username,
             $form->email
         );
-        $this->repository->save($user);
+        $this->transaction->wrap(function () use ($user, $form) {
+            $this->repository->save($user);
+            $this->roles->assign($user->id, $form->role);
+        });
     }
 
     public function remove($id): void
