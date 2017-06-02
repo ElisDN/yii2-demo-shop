1a6b5790f1904efaeb4c0383a1b901ffa858f777
ElisDN <mail@elisdn.ru>
2017-06-02 11:54:51 +0300

    Added roles console controller

---
+++ console/controllers/RoleController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace console\controllers;
+
+use shop\entities\User\User;
+use shop\services\manage\UserManageService;
+use Yii;
+use yii\console\Controller;
+use yii\console\Exception;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Interactive console roles manager
+ */
+class RoleController extends Controller
+{
+    private $service;
+
+    public function __construct($id, $module, UserManageService $service, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->service = $service;
+    }
+
+    /**
+     * Adds role to user
+     */
+    public function actionAssign(): void
+    {
+        $username = $this->prompt('Username:', ['required' => true]);
+        $user = $this->findModel($username);
+        $role = $this->select('Role:', ArrayHelper::map(Yii::$app->authManager->getRoles(), 'name', 'description'));
+        $this->service->assignRole($user->id, $role);
+        $this->stdout('Done!' . PHP_EOL);
+    }
+
+    private function findModel($username): User
+    {
+        if (!$model = User::findOne(['username' => $username])) {
+            throw new Exception('User is not found');
+        }
+        return $model;
+    }
+}
