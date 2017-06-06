d8e7b0393ec299a9f57b5ea8f8cfb30335f6f2c8
ElisDN <mail@elisdn.ru>
2017-06-06 19:29:20 +0300

    Installed queue library

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -20,6 +20,9 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'panels' => [
+            'queue' => 'yii\queue\debug\Panel',
+        ],
     ];
 
     $config['bootstrap'][] = 'gii';
