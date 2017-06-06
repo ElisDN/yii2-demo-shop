a88ac9ba504948fe1d7bc9dc86d2f267ee5ad399
ElisDN <mail@elisdn.ru>
2017-06-06 17:44:58 +0300

    Added lazy event listener resolving

--- shop/dispatchers/SimpleEventDispatcher.php
+++ shop/dispatchers/SimpleEventDispatcher.php
@@ -2,12 +2,16 @@
 
 namespace shop\dispatchers;
 
+use yii\di\Container;
+
 class SimpleEventDispatcher implements EventDispatcher
 {
+    private $container;
     private $listeners;
 
-    public function __construct(array $listeners)
+    public function __construct(Container $container, array $listeners)
     {
+        $this->container = $container;
         $this->listeners = $listeners;
     }
 
@@ -15,9 +19,15 @@ class SimpleEventDispatcher implements EventDispatcher
     {
         $eventName = get_class($event);
         if (array_key_exists($eventName, $this->listeners)) {
-            foreach ($this->listeners[$eventName] as $listener) {
+            foreach ($this->listeners[$eventName] as $listenerClass) {
+                $listener = $this->resolveListener($listenerClass);
                 $listener($event);
             }
         }
+    }
+
+    private function resolveListener($listenerClass): callable
+    {
+        return [$this->container->get($listenerClass), 'handle'];
     }
 }
