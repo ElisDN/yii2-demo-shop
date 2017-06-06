1388ccdae56d9a60d8d908d6625872a7486186cc
ElisDN <mail@elisdn.ru>
2017-06-06 17:38:14 +0300

    Added signup events listeners

--- shop/useCases/auth/SignupService.php
+++ shop/useCases/auth/SignupService.php
@@ -7,36 +7,28 @@ use shop\dispatchers\EventDispatcher;
 use shop\entities\User\User;
 use shop\forms\auth\SignupForm;
 use shop\repositories\UserRepository;
-use shop\services\newsletter\Newsletter;
 use shop\services\RoleManager;
 use shop\services\TransactionManager;
 use shop\useCases\auth\events\UserSignUpConfirmed;
 use shop\useCases\auth\events\UserSignUpRequested;
-use yii\mail\MailerInterface;
 
 class SignupService
 {
-    private $mailer;
     private $users;
     private $roles;
     private $transaction;
-    private $newsletter;
     private $dispatcher;
 
     public function __construct(
         UserRepository $users,
-        MailerInterface $mailer,
         RoleManager $roles,
         TransactionManager $transaction,
-        Newsletter $newsletter,
         EventDispatcher $dispatcher
     )
     {
-        $this->mailer = $mailer;
         $this->users = $users;
         $this->roles = $roles;
         $this->transaction = $transaction;
-        $this->newsletter = $newsletter;
         $this->dispatcher = $dispatcher;
     }
 
@@ -48,25 +40,11 @@ class SignupService
             $form->phone,
             $form->password
         );
-
         $this->transaction->wrap(function () use ($user) {
             $this->users->save($user);
             $this->roles->assign($user->id, Rbac::ROLE_USER);
         });
-
         $this->dispatcher->dispatch(new UserSignUpRequested($user));
-
-        $sent = $this->mailer
-            ->compose(
-                ['html' => 'auth/signup/confirm-html', 'text' => 'auth/signup/confirm-text'],
-                ['user' => $user]
-            )
-            ->setTo($form->email)
-            ->setSubject('Signup confirm for ' . \Yii::$app->name)
-            ->send();
-        if (!$sent) {
-            throw new \RuntimeException('Email sending error.');
-        }
     }
 
     public function confirm($token): void
@@ -77,9 +55,6 @@ class SignupService
         $user = $this->users->getByEmailConfirmToken($token);
         $user->confirmSignup();
         $this->users->save($user);
-
         $this->dispatcher->dispatch(new UserSignUpConfirmed($user));
-
-        $this->newsletter->subscribe($user->email);
     }
 }
