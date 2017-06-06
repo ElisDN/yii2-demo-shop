aecca914d15c0c03ad40513c1072f324ae1b0197
ElisDN <mail@elisdn.ru>
2017-06-06 21:57:03 +0300

    Added product appearing notification job

--- shop/listeners/Shop/Product/ProductAppearedInStockListener.php
+++ shop/listeners/Shop/Product/ProductAppearedInStockListener.php
@@ -3,52 +3,24 @@
 namespace shop\listeners\Shop\Product;
 
 use shop\entities\Shop\Product\events\ProductAppearedInStock;
-use shop\entities\Shop\Product\Product;
-use shop\entities\User\User;
+use shop\jobs\Shop\Product\ProductAvailabilityNotification;
 use shop\repositories\UserRepository;
-use yii\base\ErrorHandler;
-use yii\mail\MailerInterface;
+
+use yii\queue\Queue;
 
 class ProductAppearedInStockListener
 {
-    private $users;
-    private $mailer;
-    private $errorHandler;
+    private $queue;
 
-    public function __construct(UserRepository $users, MailerInterface $mailer, ErrorHandler $errorHandler)
+    public function __construct(UserRepository $users, Queue $queue)
     {
-        $this->users = $users;
-        $this->mailer = $mailer;
-        $this->errorHandler = $errorHandler;
+        $this->queue = $queue;
     }
 
     public function handle(ProductAppearedInStock $event): void
     {
         if ($event->product->isActive()) {
-            foreach ($this->users->getAllByProductInWishList($event->product->id) as $user) {
-                if ($user->isActive()) {
-                    try {
-                        $this->sendEmailNotification($user, $event->product);
-                    } catch (\Exception $e) {
-                        $this->errorHandler->handleException($e);
-                    }
-                }
-            }
-        }
-    }
-
-    private function sendEmailNotification(User $user, Product $product): void
-    {
-        $sent = $this->mailer
-            ->compose(
-                ['html' => 'shop/wishlist/available-html', 'text' => 'shop/wishlist/available-text'],
-                ['user' => $user, 'product' => $product]
-            )
-            ->setTo($user->email)
-            ->setSubject('Product is available')
-            ->send();
-        if (!$sent) {
-            throw new \RuntimeException('Email sending error to ' . $user->email);
+            $this->queue->push(new ProductAvailabilityNotification($event->product));
         }
     }
 }
