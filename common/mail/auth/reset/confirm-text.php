9fa87f2cf792a084bf8f01321ec324dd38d6f725
ElisDN <mail@elisdn.ru>
2017-06-07 13:27:47 +0300

    Added async event dispatcher

--- common/mail/auth/reset/confirm-text.php
+++ common/mail/auth/reset/confirm-text.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 /* @var $user \shop\entities\User\User */
 
-$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['auth/reset/confirm', 'token' => $user->password_reset_token]);
+$resetLink = Yii::$app->get('frontendUrlManager')->createAbsoluteUrl(['auth/reset/confirm', 'token' => $user->password_reset_token]);
 ?>
 Hello <?= $user->username ?>,
 
