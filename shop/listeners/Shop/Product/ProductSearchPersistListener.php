860ca855b3b183c68cad55f457d161faca7bf87f
ElisDN <mail@elisdn.ru>
2017-06-07 15:10:53 +0300

    Added products caching management

--- shop/listeners/Shop/Product/ProductSearchPersistListener.php
+++ shop/listeners/Shop/Product/ProductSearchPersistListener.php
@@ -5,14 +5,18 @@ namespace shop\listeners\Shop\Product;
 use shop\entities\Shop\Product\Product;
 use shop\repositories\events\EntityPersisted;
 use shop\services\search\ProductIndexer;
+use yii\caching\Cache;
+use yii\caching\TagDependency;
 
 class ProductSearchPersistListener
 {
     private $indexer;
+    private $cache;
 
-    public function __construct(ProductIndexer $indexer)
+    public function __construct(ProductIndexer $indexer, Cache $cache)
     {
         $this->indexer = $indexer;
+        $this->cache = $cache;
     }
 
     public function handle(EntityPersisted $event): void
@@ -23,6 +27,7 @@ class ProductSearchPersistListener
             } else {
                 $this->indexer->remove($event->entity);
             }
+            TagDependency::invalidate($this->cache, ['products']);
         }
     }
 }
