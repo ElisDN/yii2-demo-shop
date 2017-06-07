919f653ea573159666d6c536aefb115bbb704673
ElisDN <mail@elisdn.ru>
2017-06-07 15:10:53 +0300

    Added products caching management

--- shop/listeners/Shop/Product/ProductSearchRemoveListener.php
+++ shop/listeners/Shop/Product/ProductSearchRemoveListener.php
@@ -5,20 +5,25 @@ namespace shop\listeners\Shop\Product;
 use shop\entities\Shop\Product\Product;
 use shop\repositories\events\EntityRemoved;
 use shop\services\search\ProductIndexer;
+use yii\caching\Cache;
+use yii\caching\TagDependency;
 
 class ProductSearchRemoveListener
 {
     private $indexer;
+    private $cache;
 
-    public function __construct(ProductIndexer $indexer)
+    public function __construct(ProductIndexer $indexer, Cache $cache)
     {
         $this->indexer = $indexer;
+        $this->cache = $cache;
     }
 
     public function handle(EntityRemoved $event): void
     {
         if ($event->entity instanceof Product) {
             $this->indexer->remove($event->entity);
+            TagDependency::invalidate($this->cache, ['products']);
         }
     }
 }
