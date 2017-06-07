ea144f87fb7e2dc9a6b0ad75dc72543d070d7963
ElisDN <mail@elisdn.ru>
2017-06-07 15:08:09 +0300

    Added categories caching management
    Added categories caching management *

--- frontend/controllers/MarketController.php
+++ frontend/controllers/MarketController.php
@@ -4,6 +4,7 @@ namespace frontend\controllers;
 
 use shop\entities\Shop\Product\Product;
 use shop\services\yandex\YandexMarket;
+use yii\caching\TagDependency;
 use yii\helpers\Url;
 use yii\web\Controller;
 use yii\web\Response;
@@ -24,7 +25,7 @@ class MarketController extends Controller
             return $this->generator->generate(function (Product $product) {
                 return Url::to(['/shop/catalog/product', 'id' => $product->id], true);
             });
-        });
+        }, null, new TagDependency(['tags' => ['categories']]));
 
         return \Yii::$app->response->sendContentAsFile($xml, 'yandex-market.xml', [
             'mimeType' => 'application/xml',
