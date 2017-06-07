64f5b7464835dbb07c7afc145368ca2985051f74
ElisDN <mail@elisdn.ru>
2017-06-07 13:27:47 +0300

    Added async event dispatcher

--- shop/listeners/Shop/Product/ProductAppearedInStockListener.php
+++ shop/listeners/Shop/Product/ProductAppearedInStockListener.php
@@ -3,24 +3,53 @@
 namespace shop\listeners\Shop\Product;
 
 use shop\entities\Shop\Product\events\ProductAppearedInStock;
-use shop\jobs\Shop\Product\ProductAvailabilityNotification;
+use shop\entities\Shop\Product\Product;
+use shop\entities\User\User;
 use shop\repositories\UserRepository;
 
-use yii\queue\Queue;
+use yii\base\ErrorHandler;
+use yii\mail\MailerInterface;
 
 class ProductAppearedInStockListener
 {
-    private $queue;
+    private $users;
+    private $mailer;
+    private $errorHandler;
 
-    public function __construct(UserRepository $users, Queue $queue)
+    public function __construct(UserRepository $users, MailerInterface $mailer, ErrorHandler $errorHandler)
     {
-        $this->queue = $queue;
+        $this->users = $users;
+        $this->mailer = $mailer;
+        $this->errorHandler = $errorHandler;
     }
 
     public function handle(ProductAppearedInStock $event): void
     {
         if ($event->product->isActive()) {
-            $this->queue->push(new ProductAvailabilityNotification($event->product));
+            foreach ($this->users->getAllByProductInWishList($event->product->id) as $user) {
+                if ($user->isActive()) {
+                    try {
+                        $this->sendEmailNotification($user, $event->product);
+                    } catch (\Exception $e) {
+                        $this->errorHandler->handleException($e);
+                    }
+                }
+            }
+        }
+    }
+
+    private function sendEmailNotification(User $user, Product $product): void
+    {
+        $sent = $this->mailer
+            ->compose(
+                ['html' => 'shop/wishlist/available-html', 'text' => 'shop/wishlist/available-text'],
+                ['user' => $user, 'product' => $product]
+            )
+            ->setTo($user->email)
+            ->setSubject('Product is available')
+            ->send();
+        if (!$sent) {
+            throw new \RuntimeException('Email sending error to ' . $user->email);
         }
     }
 }
